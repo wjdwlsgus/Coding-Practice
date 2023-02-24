9507ecea44b9920e3d835b8454cdb2edf222b108
wjdwlsgus <97034852+wjdwlsgus@users.noreply.github.com>
2023-02-24 18:35:23 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,7 +1,7 @@
-num = [i for i in range(1,31)]
+n = [i for i in range(1,31)]
 
-for j in range(28):
-  data = int(input())
-  num.remove(data)
-print(min(num))
-print(max(num))
+for _ in range(28):
+    data = int(input())
+    n.remove(data)
+print(min(n))
+print(max(n))
